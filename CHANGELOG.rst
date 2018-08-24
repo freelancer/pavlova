813bd17141f6b474eb0bdf071c30db04898038b2
Chris Trotman <chris@freelancer.com>
2018-08-24 10:41:18 +1000

    Throw a PavlovaParsingError with context information on parsing errors

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -6,6 +6,8 @@ Changelog
 
 Added support for using Pavlova with Flask
 Added support for Python 3.6
+Added PavlovaParsingError, which is thrown when a ValueError or TypeError is
+    raised while parsing
 
 0.1.0 (2018-02-22)
 ++++++++++++++++++
--- pavlova/base.py
+++ pavlova/base.py
@@ -1,7 +1,7 @@
 "The abstract class for the Pavlova class"
 
 from abc import ABC, abstractmethod
-from typing import Any, Dict, Type, TypeVar
+from typing import Any, Type, TypeVar, Mapping, Optional, Tuple
 
 T = TypeVar('T')  # pylint: disable=invalid-name
 
@@ -10,12 +10,16 @@ class BasePavlova(ABC):
     "The base pavlova class. Use the pavlova.Pavlova class instead"
     @abstractmethod
     def from_mapping(self,
-                     input_mapping: Dict[Any, Any],
-                     model_class: Type[T]) -> T:
+                     input_mapping: Mapping[Any, Any],
+                     model_class: Type[T],
+                     path: Optional[Tuple[str, ...]] = None) -> T:
         "Parse from a Mapping and return a model_class"
         pass
 
     @abstractmethod
-    def parse_field(self, input_value: Any, field_type: Type) -> Any:
+    def parse_field(self,
+                    input_value: Any,
+                    field_type: Type,
+                    path: Tuple[str, ...]) -> Any:
         "Parse a particular field with type field_type"
         pass
